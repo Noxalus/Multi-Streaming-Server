514d3b9fb6555c081a0a0e43164463c4eba1cad2
Noxalus <noxalus@gmail.com>
2016-04-02 13:24:11 +0200

    Updated the bootstrap script to install missing libs and copy some Nginx files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
 
 sudo -s
-apt-get update
-apt-get install build-essential libpcre3 libpcre3-dev libssl-dev unzip -y
-wget http://nginx.org/download/nginx-1.9.5.tar.gz
-wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
-tar -zxvf nginx-1.9.5.tar.gz
-unzip master.zip
-cd nginx-1.9.5
-./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
-make
-make install
-/usr/local/nginx/sbin/nginx
 
-# TODO
-# copy nginx.conf template
-# change variables with sed => sed -i "s/{{YOUTUBE_API_KEY}}/$YOUTUBE_API_KEY/" nginx.conf
+nginx_path=/usr/bin/nginx
+if [ ! -e $nginx_path ]; then
+    add-apt-repository ppa:mc3man/trusty-media
+    apt-get update
+    apt-get install build-essential libpcre3 libpcre3-dev openssl libssl-dev unzip libaio1 ffmpeg -y
+    wget http://nginx.org/download/nginx-1.9.5.tar.gz
+    wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
+    tar -zxvf nginx-1.9.5.tar.gz
+    unzip master.zip
+    cd nginx-1.9.5
+    ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
+    make
+    make install
+
+    cd /home/vagrant    
+
+    # Remove downloaded archives
+    rm master.zip nginx-1.9.5.tar.gz
+
+    # Remove folder used to build Nginx
+    rm -rf nginx-1.9.5 nginx-rtmp-module-master
+
+
+    # Create a symlink for nginx
+    ln -fs /usr/local/nginx/sbin/nginx $nginx_path
+fi
+
+# Copy nginx conf files
+cp -R /vagrant/nginx/html /usr/local/nginx
+cp /vagrant/nginx/conf/nginx.conf /usr/local/nginx/conf
+
+# TODO: Launch a script to replace variables in Nginx conf file
+
+# Stop Nginx if it's running
+if [ -e /usr/local/nginx/logs/nginx.pid ]; then
+    echo "Stop Nginx server"
+    nginx -s stop
+fi
+
+# Launch Nginx
+nginx
+echo "Nginx is ready to use"
