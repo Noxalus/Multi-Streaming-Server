0e57701ba0af43c9db8e9fee2b2096e7034ce524
Noxalus <noxalus@gmail.com>
2018-03-10 01:12:02 +0100

    Update Nginx restart script.

--- nginx/script/restart.sh
+++ nginx/script/restart.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-# Stop Nginx if it's running
+# Stop Nginx if it's already running
 if [ -e /usr/local/nginx/logs/nginx.pid ]; then
-    echo "Stop Nginx server"
     nginx -s stop
 fi
 
 # Launch Nginx
 nginx
-echo "Nginx is ready to use"
+
+# Make sure Nginx configuration file is correct
+/usr/sbin/nginx -t -c /usr/local/nginx/conf/nginx.conf
+
+if [[ $? != 0 ]]; then 
+	echo "Error: Nginx's configuration file contains some errors."
+	exit 1; 
+fi
+
+# TODO: Make sure Nginx is running or display the error message
+if [ -e /usr/local/nginx/logs/nginx.pid ]; then
+	echo "Nginx is ready to use."
+else
+	echo "Nginx is not running, please check the nginx.conf configuration file is correct.\n
+		  Please check the content of the /usr/local/nginx/logs/error.log file."
+	
+	exit 1;
+fi
